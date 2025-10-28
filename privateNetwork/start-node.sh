ce95459449f17e42380dc98e1e1dca8c602cf4ab
Subhani Stark <smartboysubani2@gmail.com>
2025-10-28 17:03:28 +0530

    fixed

--- privateNetwork/start-node.sh
+++ privateNetwork/start-node.sh
@@ -8,15 +8,35 @@ fi
 
 # Start the Geth node
 echo "Starting Geth node $NODE_ID..."
-geth --datadir /app/data \
-  --port $P2P_PORT \
-  --networkid $NETWORK_ID \
-  --http --http.addr 0.0.0.0 --http.port $HTTP_PORT \
-  --http.api "eth,net,web3,personal,miner,admin" \
-  --http.corsdomain "*" \
-  --allow-insecure-unlock \
-  --unlock $UNLOCK_ADDRESS \
-  --password /app/password.txt \
-  --mine \
-  --miner.etherbase $MINER_ADDRESS \
-  --ipcpath /tmp/geth_node$NODE_ID.ipc
+
+# Check if this is node2 with bootnode
+if [ "$NODE_ID" = "2" ]; then
+    # Node 2 without mining, with bootnode
+    geth --datadir /app/data \
+      --port $P2P_PORT \
+      --networkid $NETWORK_ID \
+      --nat extip:0.0.0.0 \
+      --http --http.addr 0.0.0.0 --http.port $HTTP_PORT \
+      --http.api eth,net,web3,personal,miner,admin \
+      --http.corsdomain '*' \
+      --allow-insecure-unlock \
+      --unlock $UNLOCK_ADDRESS \
+      --password /app/password.txt \
+      --ipcpath /tmp/geth_node$NODE_ID.ipc \
+      --bootnodes $BOOTNODE_URL
+else
+    # Node 1 with mining
+    geth --datadir /app/data \
+      --port $P2P_PORT \
+      --networkid $NETWORK_ID \
+      --nat extip:0.0.0.0 \
+      --http --http.addr 0.0.0.0 --http.port $HTTP_PORT \
+      --http.api eth,net,web3,personal,miner,admin \
+      --http.corsdomain '*' \
+      --allow-insecure-unlock \
+      --unlock $UNLOCK_ADDRESS \
+      --password /app/password.txt \
+      --ipcpath /tmp/geth_node$NODE_ID.ipc \
+      --mine \
+      --miner.etherbase $MINER_ADDRESS
+fi
